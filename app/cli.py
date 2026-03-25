c051a3f8625f2320d86c0e1b9115cec2f39705cf
Mickel Kelly <mickelkelly04@gmail.com>
2026-03-25 18:20:00 -0400

    added seed script for bob/bobpass

---
+++ app/cli.py
@@ -0,0 +1,37 @@
+import typer 
+from sqlmodel import Session, select
+from app.database import engine, create_db_and_tables
+from app.models.user import User
+from app.utilites.security import encrypt_password
+
+cli = type.Typer() 
+
+@cli.command() 
+def init(): 
+    create_db_and_tables() 
+    typer.echo("Tables created") 
+
+@cli.command() 
+def seed(): 
+    with Session(engine) as session
+        existing = session.exec(
+            select(User).where(User.username == "bob")
+        ).first() 
+
+        if existing: 
+            typer.echo(" Bob already exists, skipping.")
+            return
+        
+        bob = User(
+            username="bob",
+            email="bob@mail.com",
+            password=encrypt_password("bobpass"),
+            role="admin"
+        ) 
+
+        session.add(bob)
+        session.commit()
+        typer.echo("Seeded: bob / bobpass (admin)")
+
+if __name__ == "__main__": 
+    cli() 
